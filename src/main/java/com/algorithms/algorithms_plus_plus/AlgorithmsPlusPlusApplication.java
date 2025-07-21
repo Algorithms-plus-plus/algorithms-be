73251cfa0a09be77af29537609adf3ddd0b4f115
Tazzmanian <banskaliqtabg@yahoo.com>
2025-07-21 17:40:52 +0300

    init project

---
+++ src/main/java/com/algorithms/algorithms_plus_plus/AlgorithmsPlusPlusApplication.java
@@ -0,0 +1,13 @@
+package com.algorithms.algorithms_plus_plus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmsPlusPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorithmsPlusPlusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algorithms/algorithms_plus_plus/AlgorithmsPlusPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algorithms.algorithms_plus_plus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmsPlusPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
